0c7135ae176e738e3057c8202ed3a69e03b6e622
FearDread <ghaptonstall@gmail.com>
2024-05-25 09:38:33 -0700

    --add cache dir for ui changes

---
+++ .cache/admin/src/extensions/_local-entry.ts
@@ -0,0 +1,10 @@
+import Widget0, { config as widgetConfig0 } from "./widgets/onboarding-flow/onboarding-flow"
+
+const LocalEntry = {
+  identifier: "local",
+  extensions: [
+    { Component: Widget0, config: { ...widgetConfig0, type: "widget" } }
+  ],
+}
+
+export default LocalEntry
