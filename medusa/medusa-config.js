76ba40991390d804d84de4d58a85ed4deb6a1fa3
FearDread <ghaptonstall@gmail.com>
2024-05-27 17:01:04 -0700

    --add needed docker file and compose and update medusa-config

--- medusa/medusa-config.js
+++ medusa/medusa-config.js
@@ -21,17 +21,6 @@ try {
   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
 } catch (e) {}
 
-// CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
-
-// CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
-
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
-
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
-
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
@@ -41,18 +30,17 @@ const plugins = [
       upload_dir: "uploads",
     },
   },
-  {
-    resolve: "@medusajs/admin",
-    /** @type {import('@medusajs/admin').PluginOptions} */
-    options: {
-      autoRebuild: true,
-      develop: {
-        open: process.env.OPEN_BROWSER !== "false",
-      },
-    },
-  },
+  // Uncomment to add Stripe support.
+  // You can create a Stripe account via: https://stripe.com
+  // {
+  //   resolve: `medusa-payment-stripe`,
+  //   options: {
+  //     api_key: STRIPE_API_KEY,
+  //     webhook_secret: STRIPE_WEBHOOK_SECRET,
+  //   },
+  // },
 ];
-
+/* Uncomment to add redis modules */
 const modules = {
   /*eventBus: {
     resolve: "@medusajs/event-bus-redis",
@@ -70,13 +58,12 @@ const modules = {
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
-  jwt_secret: process.env.JWT_SECRET || "supersecret",
-  cookie_secret: process.env.COOKIE_SECRET || "supersecret",
-  store_cors: STORE_CORS,
-  database_url: DATABASE_URL,
-  admin_cors: ADMIN_CORS,
-  // Uncomment the following lines to enable REDIS
-  redis_url: REDIS_URL
+  jwt_secret: process.env.JWT_SECRET,
+  cookie_secret: process.env.COOKIE_SECRET
+  store_cors: process.env.STORE_CORS,
+  database_url: process.env.DATABASE_URL,
+  admin_cors: process.env.ADMIN_CORS,
+  redis_url: process.env.REDIS_URL
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
